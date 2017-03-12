66ecb781e98672d342326db37bc466ef74ece9a4
Mateusz Witkowski <shopen93@gmail.com>
2017-03-12 15:38:29 +0100

    wybór trybu pracy aplikacji, okno logowania

---
+++ app/src/main/java/pl/lodz/trackingsystem/ApplicationSelection.java
@@ -0,0 +1,58 @@
+package pl.lodz.trackingsystem;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+import pl.lodz.trackingsystem.utils.AppUtils;
+
+public class ApplicationSelection extends AppCompatActivity {
+
+    private Button acceptButton;
+    private RadioGroup radioGroup;
+
+    private SharedPreferences settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application_selection);
+
+        settings = getSharedPreferences(AppUtils.PREFS_NAME, 0);
+        if(!"".equals(settings.getString("APPLICATION_MODE", ""))) {
+            // we know which app is it, redirect to next action
+            Intent intent = new Intent(ApplicationSelection.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+        acceptButton = (Button) findViewById(R.id.acceptButton);
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int chosenRadio = radioGroup.getCheckedRadioButtonId();
+                String appMode = "";
+
+                if(R.id.monitRadio == chosenRadio) {
+                    // we will monit this device and send data to server
+                    appMode = AppUtils.MONIT_APP;
+                } else {
+                    // we are admin, we only receive monits from server
+                    appMode = AppUtils.ADMIN_APP;
+                }
+
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("APPLICATION_MODE", appMode);
+                editor.commit();
+
+                Intent intent = new Intent(ApplicationSelection.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        radioGroup = (RadioGroup) findViewById(R.id.selectionGroup);
+    }
+}
--- app/src/main/java/pl/lodz/trackingsystem/utils/AppUtils.java
+++ app/src/main/java/pl/lodz/trackingsystem/utils/AppUtils.java
@@ -4,6 +4,12 @@ import android.os.Handler;
 
 public class AppUtils {
 
+    public static final String PREFS_NAME = "TrackingSystemFile";
+
+    public static final String MONIT_APP = "MONIT";
+
+    public static final String ADMIN_APP = "ADMIN";
+
     private static Handler handler = new Handler();
 
     public static void wait(int ms) {
